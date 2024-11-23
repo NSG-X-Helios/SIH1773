ce3f244c95e44b473da96782dcd9e7bf112a4768
lovelindhoni <jblovelindhoni@gmail.com>
2024-11-23 12:10:12 +0530

    Initial commit 💦

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Canvas } from "@threlte/core";
+  import Scene from "$lib/components/Scene.svelte";
+  import Modifiers from "$lib/components/Modifiers.svelte";
+</script>
+
+<main class="h-screen w-screen items-center justify-center flex">
+  <div class="h-full w-[35%]">
+    <Modifiers />
+  </div>
+  <div class="h-full w-[65%] flex items-center justify-center">
+    <Canvas>
+      <Scene />
+    </Canvas>
+  </div>
+</main>
---
+++ src/lib/components/Building.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { GLTF, useDraco } from "@threlte/extras";
+  const dracoLoader = useDraco("/decoders/");
+</script>
+
+<GLTF
+  castShadow
+  receiveShadow
+  url={"/models/Floorplan-transformed.glb"}
+  {dracoLoader}
+/>
